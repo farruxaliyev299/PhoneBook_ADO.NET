f48968dfb9bae5fc8dd2313118509e6411daf361
farruxaliyev299 <7hp245d@code.edu.az>
2023-01-16 17:51:49 +0400

    Added Task

---
+++ DataAccess/Helpers/PersonHelper.cs
@@ -0,0 +1,151 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Helpers
+{
+    public static class PersonHelper
+    {
+        private const string _CONNECTION_STRING = @"Server=DESKTOP-MLES57C;Database=PhoneBook;Integrated Security=true";
+
+        public static List<Person> GetAllConnected()
+        {
+            SqlConnection conn = new SqlConnection(_CONNECTION_STRING);
+
+            SqlCommand cmd = new SqlCommand("select * from People", conn);
+
+            if (conn.State == ConnectionState.Closed) conn.Open();
+
+            //Read Proccess
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            List<Person> people = new List<Person>();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    Person person = new Person();
+                    person.Id = Convert.ToInt32(dr["Id"]);
+                    person.FirstName = dr["FirstName"].ToString();
+                    person.LastName = dr["LastName"].ToString();
+                    person.Phone = dr["Phone"].ToString();
+                    person.Email = dr["Email"].ToString();
+
+                    people.Add(person);
+                }
+
+                return people;
+            }
+            dr.Close();
+            conn.Close();
+            return null;
+        }
+
+        public static List<Person> GetAllPeople()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("select * from People", _CONNECTION_STRING);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            var list = (from DataRow dr in dt.Rows
+                        select new Person
+                        {
+                            Id = Convert.ToInt32(dr["Id"]),
+                            FirstName = dr["FirstName"].ToString(),
+                            LastName = dr["LastName"].ToString(),
+                            Phone = dr["Phone"].ToString(),
+                            Email = dr["Email"].ToString(),
+                        }
+               ).ToList();
+
+            return list;
+        }
+
+        public static bool InsertPerson(Person person)
+        {
+            SqlConnection conn = new SqlConnection(_CONNECTION_STRING);
+
+            SqlCommand cmd = new SqlCommand("insert into People values ( @FirstName, @LastName, @Phone, @Email)", conn);
+            cmd.Parameters.Add("@Firstname", SqlDbType.NVarChar).Value = person.FirstName;
+            cmd.Parameters.Add("@Lastname", SqlDbType.NVarChar).Value = person.LastName;
+            cmd.Parameters.Add("@Phone", SqlDbType.NVarChar).Value = person.Phone;
+            cmd.Parameters.Add("@Email", SqlDbType.NVarChar).Value = person.Email;
+
+            if (conn.State == ConnectionState.Closed) conn.Open();
+
+            bool result = cmd.ExecuteNonQuery() > 0;
+
+            conn.Close();
+
+            return result;
+        }
+
+        public static bool DeletePerson(int id)
+        {
+            SqlConnection conn = new SqlConnection(_CONNECTION_STRING);
+
+            SqlCommand cmd = new SqlCommand("delete from People where Id = @id", conn);
+
+            cmd.Parameters.Add("@Id", SqlDbType.Int).Value = id;
+
+            if (conn.State == ConnectionState.Closed) conn.Open();
+
+            bool result = cmd.ExecuteNonQuery() > 0;
+
+            conn.Close();
+
+            return result;
+        }
+
+        public static List<Person> FilterPeople(string data)
+        {
+            List<Person> result = new List<Person>();
+
+            SqlConnection conn = new SqlConnection(_CONNECTION_STRING);
+
+            SqlCommand cmd = new SqlCommand();
+
+            int res;
+
+            if (int.TryParse(data, out res))
+            {
+                cmd = new SqlCommand($"select * from People where Id = {res} or FirstName = '{data}' or LastName = '{data}' or  Phone = '{data}' or Email = '{data}'", conn);
+            }
+            else
+            {
+                cmd = new SqlCommand($"select * from People where FirstName = '{data}' or LastName = '{data}' or  Phone = '{data}' or Email = '{data}'", conn);
+            }
+
+
+            if (conn.State == ConnectionState.Closed) conn.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                Person newPerson = new()
+                {
+                    Id = Convert.ToInt32(dr["Id"]),
+                    FirstName = dr["FirstName"].ToString(),
+                    LastName = dr["LastName"].ToString(),
+                    Phone = dr["Phone"].ToString(),
+                    Email = dr["Email"].ToString()
+                };
+
+                result.Add(newPerson);
+            }
+
+            dr.Close();
+            conn.Close();
+
+            return result;
+        }
+    }
+}
---
+++ DataAccess/Program.cs
@@ -0,0 +1,169 @@
+﻿using DataAccess.Helpers;
+using DataAccess.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Reflection;
+
+namespace DataAccess;
+
+internal class Program
+{
+    private const string _CONNECTION_STRING = @"Server=DESKTOP-MLES57C;Database=PhoneBook;Integrated Security=true";
+    static void Main(string[] args)
+    {
+
+        while (true)
+        {
+            Console.WriteLine("\na. Insert Data");
+            Console.WriteLine("l. List all data");
+            Console.WriteLine("s. Search data");
+            Console.WriteLine("e. Exit\n");
+
+            char userInput = Console.ReadKey(true).KeyChar;
+
+            switch (userInput)
+            {
+                case 'a':
+                    AddPerson();
+                    break;
+                case 'l':
+                    ListAllPeople();
+                    break;
+                case 's':
+                    FilteredList();
+                    break;
+                case 'e':
+                    return;
+                default:
+                    Console.WriteLine("Try Again");
+                    break;
+            }
+        }
+
+        //var people = GetAllConnected();
+
+        //foreach (var person in people)
+        //{
+        //    Console.WriteLine(new String('_',50));
+        //    Console.WriteLine("\n");
+        //    foreach (PropertyInfo prop in person.GetType().GetProperties())
+        //    {
+        //        Console.WriteLine($"{prop.Name, -20} : {prop.GetValue(person)}");
+        //    }
+        //    Console.WriteLine("\n");
+        //}
+
+
+    }
+
+    public static void AddPerson()
+    {
+        Console.Write("Enter the name: ");
+        string nameInput = Console.ReadLine();
+
+        Console.Write("Enter the surname: ");
+        string lastNameInput = Console.ReadLine();
+
+        Console.Write("Enter the Phone: ");
+        string phoneInput = Console.ReadLine();
+
+        Console.Write("Enter the Email: ");
+        string emailInput = Console.ReadLine();
+
+        Console.WriteLine("Do you want to save? (Y/N)");
+
+        ConsoleKey key = Console.ReadKey(true).Key;
+
+        while(key != ConsoleKey.Y && key != ConsoleKey.N)
+        {
+            key = Console.ReadKey(true).Key;
+        }
+
+        if(key == ConsoleKey.N)
+        {
+            Console.WriteLine("Person draft deleted");
+            return;
+        }
+
+        Person newPerson = new()
+        {
+            FirstName = nameInput,
+            LastName = lastNameInput,
+            Phone = phoneInput,
+            Email = emailInput
+        };
+
+        Console.WriteLine(PersonHelper.InsertPerson(newPerson) ? "\nPerson added" : "\nProblems occured when inserting data");
+    }
+
+    public static void ListAllPeople(List<Person> people = null)
+    {
+        if (people is null)
+        {
+            people = PersonHelper.GetAllPeople();
+        }
+
+        Console.WriteLine();
+        Console.Write(new String(' ', 10) + "Id" + new String(' ', 23));
+        Console.Write("FirstName" + new String(' ', 16));
+        Console.Write("LastName" + new String(' ', 17));
+        Console.Write("Phone" + new String(' ', 20));
+        Console.Write("Email" + new String(' ', 20));
+
+        Console.WriteLine("\n" + new String('_', 150));
+
+        int spaceSize = 25;
+        foreach (var person in people)
+        {
+            Console.Write(new String(' ', 10));
+            Console.Write(person.Id + new String(' ', spaceSize - person.Id.ToString().Length));
+
+            Console.Write(person.FirstName + new String(' ', spaceSize - person.FirstName.ToString().Length));
+
+            Console.Write(person.LastName + new String(' ', spaceSize - person.LastName.ToString().Length));
+
+            Console.Write(person.Phone + new String(' ', spaceSize - person.Phone.ToString().Length));
+
+            Console.Write(person.Email);
+
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        ConsoleKey listInputs;
+        do
+        {
+            Console.WriteLine("\nd. Delete data");
+            Console.WriteLine("m. Return to main menu\n");
+            listInputs = Console.ReadKey(true).Key;
+        }
+        while (listInputs != ConsoleKey.D && listInputs != ConsoleKey.M);
+
+        if (listInputs == ConsoleKey.D)
+        {
+            Console.Write("Enter Id: ");
+            int deleteInput = int.Parse(Console.ReadLine());
+
+            if (PersonHelper.DeletePerson(deleteInput)) Console.WriteLine("\nPerson deleted\n");
+            else Console.WriteLine("\nPerson didn't found\n");
+            return;
+        }
+        else if (listInputs == ConsoleKey.M)
+        {
+            Console.Clear();
+            return;
+        }
+    }
+
+    public static void FilteredList()
+    {
+        Console.Write("\nEnter filter input: ");
+        string filterInput = Console.ReadLine();
+
+        List<Person> people = PersonHelper.FilterPeople(filterInput);
+
+        ListAllPeople(people);
+    }
+
+
+}
